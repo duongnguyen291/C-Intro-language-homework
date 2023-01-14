46a7a6589221bf1cfbd8df8379567c8a743ce885
duongnguyen291 <2901nguyendinhduong@gmail.com>
2023-01-14 23:10:47 +0700

    add only in folder 12

--- homework/week12/homework1.c
+++ homework/week12/homework1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include <stdlab.h>
+#include <stdlib.h>
 int main(){
     int n;
-    printf("\nEnter the number");
+    printf("\nEnter the number: ");
     scanf("%d",&n);
     printf("\nThe number is : %d",n);
+    printf("\nHehehe! ");
+    for(int i = 0 ; i < 10; i++){
+        printf("\n%d. I love you! ", i + 1);
+    }
+
     return 0;
 }
