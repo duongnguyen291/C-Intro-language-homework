821243c8cfe7c5b38b4a21ce14a297db2c8eb89e
duongnguyen291 <2901nguyendinhduong@gmail.com>
2023-02-01 22:42:27 +0700

    week13-1: start

--- homework/week13/homework1.c
+++ homework/week13/homework1.c
@@ -1,9 +1,17 @@
-#include <stidio.h>
+#include <stdio.h>
+int convert(int *n);
 int main(){
+unsigned int n;
+printf("\nEnter the number n: ");
+scanf("%d",&n);
+printf("\nConvert number is: %d",convert(&n));
+return  0;    
+}
+int convert(int *n){
+    int u;
+    int unit=0;
+    while(n>0){
 
-
-
-
-
-    
+    }
+    return u;
 }
