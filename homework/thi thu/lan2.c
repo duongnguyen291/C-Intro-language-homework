2ea3d9012d9c9f6f700d6daf5027f85c7a47d4fe
Nguyễn Đình Dương <116486270+duongnguyen291@users.noreply.github.com>
2023-02-24 10:38:00 +0700

    update lan 2.c

--- homework/thi thu/lan2.c
+++ homework/thi thu/lan2.c
@@ -4,41 +4,12 @@
 #include <ctype.h>
 void get_by_range(int *n, int a, int b);
 void getpositive(int *n);
-<<<<<<< HEAD
+
 void lietke(int status[],int a){
     for(int i = 0; i < a; i++){
         printf("%5d",status[i]);
     }
 }
-//MAIN
-int main(){
-  //khai bao bien
-  int home;
-  int n1;
-  int arr[20];
-  int count[20][20];
-  //lenh
-  do{
-    printf("\nMenu\n\t1.Frequenct\n\t2.Fibonacci\n\t3.Password\n\t4.Date\n\t5.Quit\nYour choice is:    ");
-    scanf("%d",&home);
-    switch(home){
-    case 1:;
-        printf("\nEnter the integer number (0 < n <=20): ");
-        scanf("%d",&n1);
-        for (int i = 0; i < n1; i++){
-            scanf("%d",&arr[i]);
-        }
-        for(int i = 0; i < n1 ;i++){
-          
-        }
-        
-       break;
-    case 2:
-
-      break;
-    case 3:
-
-=======
 //MAIN
 int find_max_number(int str[],int n){
     int count = str[0];
@@ -133,7 +104,6 @@ int main(){
         }
         }while(check_password(password) == 0);
         printf("\nMat khau da du manh! ");
->>>>>>> 8b3f237aadd8807020e109dec29c68905ed03cd9
       break;
     case 4:
 
